33b5c97f139434a343af60b4229c9247aaa631b1
P10r10 <asantiago@keemail.me>
2022-12-11 20:42:07 +0000

    test intellij

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,11 @@ public class Main {
         l.add("000");
         l.add("111");
         l.add("222");
+        l.add("22");
         l.add("333");
         l.remove("111");
         System.out.println(l.contains("111"));
         System.out.println(l.size());
+        //aaa
     }
 }
